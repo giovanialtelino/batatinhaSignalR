c123c04602b5ff23d2361a9ca50ef52e53d4240a
giovanialtelino <giovanialtelino@hotmail.com>
2022-07-27 02:04:07 -0300

    function to list groups

--- Consumer/HostedService/KafkaReader.cs
+++ Consumer/HostedService/KafkaReader.cs
@@ -25,18 +25,21 @@ namespace BatatinhaSignalR.HostedService
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var activeConsumer = ListConsumerGroupsOpen("localhost:9092", "marvel2");
+
             Task.Run(async () =>
             {
                 var config = new ConsumerConfig
                 {
                     BootstrapServers = "localhost:9092",
-                    ClientId = "batima2",
-                    GroupId = "marvel"
+                    ClientId = "batima5",
+                    GroupId = "marvel2",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
                 };
 
                 var schemaRegistryConfig = new SchemaRegistryConfig
                 {
-                    Url = "localhost:8081"
+                    Url = "localhost:8081",
                 };
 
                 using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
@@ -73,5 +76,49 @@ namespace BatatinhaSignalR.HostedService
 
             return Task.CompletedTask;
         }
+
+        private static string ListConsumerGroups(string bootstrapServers)
+        {
+            string s = ($"Consumer Groups:");
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                var groups = adminClient.ListGroups(TimeSpan.FromSeconds(10));
+                foreach (var g in groups)
+                {
+                    s += "\r\n" + ($"  Group: {g.Group} {g.Error} {g.State}");
+                    s += "\r\n" + ($"  Broker: {g.Broker.BrokerId} {g.Broker.Host}:{g.Broker.Port}");
+                    s += "\r\n" + ($"  Protocol: {g.ProtocolType} {g.Protocol}");
+                    s += "\r\n" + ($"  Members:");
+                    foreach (var m in g.Members)
+                    {
+                        s += "\r\n" + ($"    {m.MemberId} {m.ClientId} {m.ClientHost}");
+                        s += "\r\n" + ($"    Metadata: {m.MemberMetadata.Length} bytes");
+                        s += "\r\n" + ($"    Assignment: {m.MemberAssignment.Length} bytes");
+                    }
+                }
+            }
+            return s;
+        }
+
+        private static bool ListConsumerGroupsOpen(string bootstrapServers, string connectingGroup)
+        {
+            string s = ($"Consumer Groups:");
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                var groups = adminClient.ListGroups(TimeSpan.FromSeconds(10));
+                foreach (var g in groups)
+                {
+                    if (g.Group == connectingGroup)
+                    {
+                        foreach (var m in g.Members)
+                        {
+                            if (m.MemberId != null) return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
     }
 }
